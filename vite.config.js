a34afdca21a1ac933e74e213e2451dab4f2e05fa
yadukrishnan <yadhu4507@gmail.com>
2024-08-17 12:39:20 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host:"0.0.0.0",
-    port:5173
-  }
+  // server:{
+  //   host:"0.0.0.0",
+  //   port:5173
+  // }
+  build: {
+    chunkSizeWarningLimit: 1000, // Set this to your desired limit in KB
+  },
 })
